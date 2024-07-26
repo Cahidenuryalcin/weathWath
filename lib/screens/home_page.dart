61c33101a2b0738ae28ac69d78573c7dafc8bda7
Cahidenuryalcin <cnuryal@gmail.com>
2024-07-27 00:58:00 +0300

    27.07.24

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:weathwath_app/models/weatherdata.dart';
+import 'package:weathwath_app/services/weather_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
-  
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  WeatherData? _result;
+  final WeatherService _weatherService = WeatherService();
+
+  void _search() async {
+    WeatherData? weatherData = await _weatherService.fetchWeather(_controller.text);
+    setState(() {
+      _result = weatherData;
+      _controller.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 20),
-            SearchBar(),
-            SizedBox(height: 20),
-            WeatherInfo(),
-            SizedBox(height: 30),
-            WeatherDetails(),
-           
+            const SizedBox(height: 50),
+            SearchBar(
+              controller: _controller,
+              onSearch: _search,
+            ),
+            const SizedBox(height: 20),
+            if (_result != null) ...[
+              WeatherInfo(result: _result!.result),
+            ] else ...[
+              const Center(child: Text("Şehir ara ve hava durumunu öğren.", style: TextStyle(fontSize: 18))),
+            ],
           ],
         ),
       ),
@@ -33,124 +48,183 @@ class _HomePageState extends State<HomePage> {
 }
 
 class SearchBar extends StatelessWidget {
-  const SearchBar({super.key});
+  final TextEditingController controller;
+  final VoidCallback onSearch;
+
+  const SearchBar({required this.controller, required this.onSearch, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Search Location',
-        hintStyle: TextStyle(color: Colors.grey[400]), // Açık gri renk
-        suffixIcon: Icon(Icons.search, color: Colors.grey[400]), // Sağ tarafta açık gri ikon
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide.none,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: 'Şehir Ara',
+          hintStyle: TextStyle(color: Colors.grey[400]),
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.search),
+            color: Colors.grey[400],
+            onPressed: onSearch,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Colors.grey[100],
         ),
-        filled: true,
-        fillColor: Colors.grey[100],
       ),
     );
   }
 }
-
 
 class WeatherInfo extends StatelessWidget {
-  const WeatherInfo({super.key});
+  final List<Result> result;
+
+  const WeatherInfo({required this.result, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(Icons.cloud, size: 100, color: Colors.blue),
-          Text(
-            'Mumbai',
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          
-           RichText(
-            text: TextSpan(
-              style: TextStyle(
-                fontSize: 80,
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
-              ),
-              children: [
-                TextSpan(
-                  text: '20',
+    return Column(
+      children: [
+        // İlk hava durumu kartı
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Konumum\n${result.first.day}',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
                 ),
-                WidgetSpan(
-                  child: Transform.translate(
-                    offset: const Offset(4, -40),
-                    child: Text(
-                      '°',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.w300,
-                        color: Colors.black,
+              ),
+              const SizedBox(height: 20),
+              Image.network(result.first.icon, width: 100, height: 100),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: const TextStyle(
+                    fontSize: 80,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: result.first.degree,
+                    ),
+                    WidgetSpan(
+                      child: Transform.translate(
+                        offset: Offset(4, -40), // const anahtar kelimesi kaldırıldı
+                        child: const Text(
+                          '°',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                result.first.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  weatherInfoColumn('Min', result.first.min),
+                  weatherInfoColumn('Max', result.first.max),
+                  weatherInfoColumn('Nem', result.first.humidity),
+                ],
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        ),
+        // Diğer günlerin hava durumu kartları
+        ...result.skip(1).map((weather) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              elevation: 5,
+              child: ListTile(
+                leading: Image.network(weather.icon, width: 50, height: 50),
+                title: Text(
+                  weather.day,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ],
-            ),),
-        ],
-      ),
+                subtitle: Text(
+                  weather.description,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                trailing: RichText(
+                  text: TextSpan(
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(text: weather.degree),
+                      WidgetSpan(
+                        child: Transform.translate(
+                          offset: Offset(2, -10), // const anahtar kelimesi kaldırıldı
+                          child: const Text(
+                            '°',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ],
     );
   }
-}
 
-class WeatherDetails extends StatelessWidget {
-  const WeatherDetails({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  Widget weatherInfoColumn(String label, String value) {
+    return Column(
       children: [
-        Column(
-          children: [
-            Text('TIME', style: TextStyle(color: Colors.grey.shade400),),
-            Text(
-              '11:25 AM',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade600),
-            ),
-          ],
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+          ),
         ),
-        Column(
-          children: [
-            Text('UV', style: TextStyle(color: Colors.grey.shade400),),
-            Text(
-              '4',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade600),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Text('% RAIN', style: TextStyle(color: Colors.grey.shade400),),
-            Text(
-              '58%',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade600),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Text('AQ', style: TextStyle(color: Colors.grey.shade400),),
-            Text(
-              '22',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey.shade600),
-            ),
-          ],
+        const SizedBox(height: 10),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
   }
 }
-
-
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -4,18 +4,24 @@ import 'package:http/http.dart' as http;
 import 'package:weathwath_app/models/weatherdata.dart';
 
 class WeatherService {
-  final String apiKey = '720e9877b47531d04b32fed39b5e2106';
+  final String baseUrl =
+      'https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=';
 
-  Future<WeatherData?> fetchWeather(String query) async {
-    final String baseUrl = 'https://api.weatherstack.com/current?access_key=$apiKey&query=$query';
-    
-    var res = await http.get(Uri.parse(baseUrl));
+  Future<WeatherData?> fetchWeather(String city) async {
+    var headers = {
+      'authorization': 'apikey 3iRur6OoX50o7AYRUTqXA9:0T2ouppJ0houRyQDxe3bnl', 
+      'content-type': 'application/json'
+    };
+
+    var res = await http.get(Uri.parse("$baseUrl$city"), headers: headers);
     if (res.statusCode == 200) {
+      debugPrint(res.body); 
       Map<String, dynamic> jsonResponse = json.decode(res.body);
       return WeatherData.fromJson(jsonResponse);
     } else {
       debugPrint("request failed: ${res.statusCode}");
     }
+
     return null;
   }
 }
